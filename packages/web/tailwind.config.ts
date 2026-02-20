44308bae7439bc2d05bb1c98d0924611b5d69f1f
Coldin04 <me@cold04.com>
2026-02-20 12:57:37 +0800

    Add spring-green color palette to Tailwind config

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -4,7 +4,23 @@ export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "spring-green": {
+          "50": "#edfff7",
+          "100": "#d5ffef",
+          "200": "#aeffdf",
+          "300": "#70ffc7",
+          "400": "#2bfda8",
+          "500": "#00ec8d",
+          "600": "#00c06e",
+          "700": "#009659",
+          "800": "#067549",
+          "900": "#07603e",
+          "950": "#003722"
+        }
+      }
+    }
   },
 
   plugins: [require("@tailwindcss/typography")]
